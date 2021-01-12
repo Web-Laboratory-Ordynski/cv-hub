194d460ed317aa21f7966028c6c1b0ac508c1ce6
AntonDeXy <podolyananton@gmail.com>
2021-01-13 00:18:48 +0200

    CH-27/connected mongodb, add logger

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const PORT = 5000
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const mongodb = require('mongodb')
 
 const baseRoutes = require('./routes/base-routes')
 
@@ -13,4 +15,17 @@ app.use(bodyParser.json())
 
 app.use('/api', baseRoutes)
 
-app.listen(PORT, () => console.log('Server started'))
+mongodb.connect(
+  `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.futoz.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`,
+  {
+    useUnifiedTopology: true
+  },
+  (err) => {
+    if (err) {
+      throw new err
+    }
+    console.log('db connected')
+    app.listen(PORT, () => console.log('Server started'))
+  }
+)
+
