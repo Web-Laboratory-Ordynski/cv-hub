ffefb3fcfbc3037b38ff38f9c8716a28a44fedcc
AntonDeXy <podolyananton@gmail.com>
2021-01-13 21:31:34 +0200

    CH-26/make-serured-route (check jwt)

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -83,6 +83,19 @@ exports.logout = async (req, res) => {
   res.sendStatus(204)
 }
 
+exports.authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if (!token) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
+    if (err) return res.sendStatus(403)
+    req.user = data.user
+    next()
+  })
+}
+
 function generateAccessToken(user, date) {
   return jwt.sign({user, createdAt: date}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15m'})
 }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -6,4 +6,6 @@ router.post('/login', userContoller.login)
 
 router.post('/register', userContoller.register)
 
+router.get('/secured-data', userContoller.authenticateToken, (req, res) => res.send('Secured data'))
+
 module.exports = router
