09115a7424ed53b24bfee7f1c650a914564883a6
saitem <gamesshooter42@gmail.com>
2021-01-14 19:01:07 +0200

    add navigation

--- client/src/components/layout/Header/Header.jsx
+++ client/src/components/layout/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
     return (
         <div>
-            Header
+            <Link to='/about'>About</Link>
+            <Link to='/home'>Home</Link>
+            <Link to='/login'>Login</Link>
+            <Link to='/signup'>Sign Up</Link>
+            <Link to='/user/profile'>Profile</Link>
+            <Link to='/user/profile/edit'>Edit profile</Link>
+            <Link to='/resume/create'>Create Resume</Link>
+            <Link to='/resume/resumes'>Resumes</Link>
         </div>
     )
 }
