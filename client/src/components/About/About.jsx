d583b8b00f8836157805567c25385c813b67415b
Sergey Tikunov <sergeytikunov@Sergeys-MacBook-Air.local>
2021-02-06 11:24:14 +0200

    CH-15/About_page

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import '.../App.css';
 
-export const About = () => {
+class App extends Component {
+  render() {
     return (
-        <div>
-            About
-        </div>
-    )
+      <div className="App">
+        <header className="App-header-about">
+          <img src={logo} className="App-logo" alt="logo" />
+          
+          <h1 className="App-title">About us</h1>
+          <h5 className="App-title-2">Just information about creaters of this site. Small, but very smart Team</h5>
+        </header>
+        <p className="App-intro">
+          Our  greate team isn't so big, course it is so small
+        </p>
+        <p className="App-title-about2">
+          Not all, but a most of our members you can see on the next image:
+        </p>
+        <img src={window.location.origin + '/BRA_Team_2021.jpg'} width="850" height="500" alt="BRA_Team_2021"/>
+      </div>
+    );
+  }
 }
+
+export default App;
