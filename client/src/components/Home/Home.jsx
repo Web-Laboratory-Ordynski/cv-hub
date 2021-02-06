15525f0c95fc188919f8650b47d7f351f934bea4
Anastasiia Gvozdieva <nastiagvozdieva@gmail.com>
2021-02-06 11:18:13 +0200

    home page

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
+import { Human } from './Human'
+import s from './home.module.css'
+
 
 export const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
+            <div className={s.header__conteiner}>
+                <h1>
+                    СОЗДАЙ РЕЗЮМЕ <br/>ДЛЯ ПОИСКА РАБОТЫ <br/>В ОНЛАЙН КОНСТРУКТОРЕ
+                </h1>
+                <Human />
+            </div>
         </div>
     )
 }
