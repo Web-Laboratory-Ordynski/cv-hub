42243e3b042276269ae48915da30b7a9ec590339
AntonDeXy <podolyananton@gmail.com>
2021-02-10 22:50:41 +0200

    remove test route, change getter for Authorization header

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -103,7 +103,7 @@ exports.editCV = async (req, res) => {
 }
 
 exports.authenticateToken = (req, res, next) => {
-  const authHeader = req.headers['authorization']
+  const authHeader = req.headers['Authorization']
   const token = authHeader && authHeader.split(' ')[1]
 
   if (!token) return res.sendStatus(401)
