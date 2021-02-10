7efeb4f6248cd8aeed33de50d5988615c816b3f3
Saitem <bondarartur@protonmail.ch>
2021-02-11 01:30:02 +0200

    added request to get new token

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,6 +2,21 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:5000/api'
 
+const getFromLs = (value) => localStorage.getItem(value)
+const setRefreshTokenToLs = value => localStorage.setItem('refreshToken', value)
+
+axios.interceptors.request.use(
+	config => {
+		const token = getFromLs('token').token
+		if (token) {
+			config.headers['Authorization'] = 'Bearer ' + token;
+		}
+		return config;
+	},
+	error => {
+		Promise.reject(error)
+	})
+
 const API = {
   	register: async user => {
 		return axios.post(baseUrl + '/register', user)
@@ -27,7 +42,26 @@ const API = {
 				return err.response.data
 			})
 	},
-	
+	getNewToken: async () => {
+		const refreshToken = getFromLs('response').refreshToken
+
+		if(refreshToken) {
+			const res = await axios.post(baseUrl + '/get-new-token', { refreshToken })
+			if(res.status === 200) {
+				setRefreshTokenToLs(res.data.accessToken)
+
+				return {
+					accessToken: res.data.accessToken,
+					user: res.data.user
+				}
+			} else {
+				return false
+			}
+		} else {
+			return false
+		}
+	}
+
 }
 
 export default API
