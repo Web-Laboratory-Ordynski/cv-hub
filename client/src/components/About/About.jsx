0f974cb88a85db82b68ca0f7bc1c4a60923a5c7e
Sergey Tikunov <sergeytikunov@Sergeys-MacBook-Air.local>
2021-02-10 21:24:05 +0200

    Final version About page

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import '../../App.css';
+import GR1 from './Images/GR-1.jpg';
+import GR2 from './Images/GR-2.jpg';
+import GR3 from './Images/GR-3.jpg';
+import GR4 from './Images/GR-4.jpg';
+import GR5 from './Images/GR-5.jpg';
+import GR6 from './Images/GR-6.jpg';
+
 
 class About extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header-about">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h5>React people</h5>
-          
+        <div className="App-header-about">
           <h1 className="App-title">About us</h1>
-          <h5 className="App-title-about2">Just information about creaters of this site. Small, but very smart Team</h5>
-        </header>
-        <p className="App-intro">
-          Our  greate team isn't so big, course it is so small
-        </p>
-        <p className="App-title-about2">
-          Not all, but a most of our members you can see on the next image:
-        </p>
+          <h5>Just information about creaters of this site. Small, but very smart Team</h5>
+        </div>
+
+        <p>Our  greate team isn't so big, course it is so small</p>
+        <p>Not all, but a most of our members you can see on the next image:</p>
         <img src={window.location.origin + '/BRA_Team_2021.jpg'} width="850" height="500" alt="BRA_Team_2021"/>
+        <p>We started in 2020 year and finished in 2021. As a result you can see this butiful and wanderful site. 
+          It is so perfect as a sunrise </p>
+
+        <div className="Images-Wrapper">
+        <h4>Other photos of our perfect team:</h4>
+        <img src={GR1} width="700" height="500" alt="GR1 image"/>
+        <p></p>
+        <img src={GR2} width="700" height="500" alt="GR2 image"/>
+        <p></p>
+        <img src={GR3} width="700" height="500" alt="GR3 image"/>
+        <p></p>
+        <img src={GR4} width="700" height="500" alt="GR4 image"/>
+        <p></p>
+        <img src={GR5} width="700" height="500" alt="GR5 image"/>
+        <p></p>
+        <h4>Our teacher:</h4>
+        <img src={GR6} width="400" height="500" alt="GR6 image"/>
+        </div>
+       
+        
       </div>
     );
   }
