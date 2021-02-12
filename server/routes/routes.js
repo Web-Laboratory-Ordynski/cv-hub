2cc20800496612551a7176fc25fb6681de6c3d2c
Saitem <bondarartur@protonmail.ch>
2021-02-12 22:32:56 +0200

    server fix

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -8,22 +8,6 @@ router.post('/register', userContoller.register)
 
 router.post('/get-new-token', userContoller.getNewTokenByRefreshToken)
 
-router.get('/secured-data', userContoller.authenticateToken, (req, res) => res.send('Secured data'))
-
-router.put('/edit-cv', userContoller.authenticateToken, userContoller.editCV)
-
-module.exports = router
-=======
-const express = require('express')
-const router = express.Router()
-const userContoller = require('../controllers/user.controller')
-
-router.post('/login', userContoller.login)
-
-router.post('/register', userContoller.register)
-
-router.post('/get-new-token', userContoller.getNewTokenByRefreshToken)
-
 router.put('/edit-cv', userContoller.authenticateToken, userContoller.editCV)
 
 module.exports = router
