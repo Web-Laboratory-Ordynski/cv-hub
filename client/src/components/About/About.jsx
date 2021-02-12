ea63a3a9262ce1c394c5486ff5b1664c53684ec0
Sergey Tikunov <sergeytikunov@Sergeys-MacBook-Air.local>
2021-02-12 16:00:18 +0200

    Linkedin added

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -7,6 +7,13 @@ import GR4 from './Images/GR-4.jpg';
 import GR5 from './Images/GR-5.jpg';
 import GR6 from './Images/GR-6.jpg';
 
+import AntonH from './Persons/Anton_H.jpeg';
+import AntonP from './Persons/Anton_P.jpeg';
+import Artur from './Persons/Artur.jpeg';
+import Dmytro from './Persons/Dmytro.jpeg';
+import Nastia from './Persons/Nastia.jpeg';
+import Serega from './Persons/Serega.jpeg';
+
 
 class About extends Component {
   render() {
@@ -20,9 +27,24 @@ class About extends Component {
         <p>Our  greate team isn't so big, course it is so small</p>
         <p>Not all, but a most of our members you can see on the next image:</p>
         <img src={window.location.origin + '/BRA_Team_2021.jpg'} width="850" height="500" alt="BRA_Team_2021"/>
-        <p>We started in 2020 year and finished in 2021. As a result you can see this butiful and wanderful site. 
+        <p>We started in 2020 year and finished in 2021. As a result you can see this beautiful and wanderful site. 
           It is so perfect as a sunrise </p>
-
+          <p></p>
+          <p>Finally, our Team:</p>
+        
+        <p><img src={Artur} width="150" height="150" alt="Artur image"/></p> 
+        <p><a href="https://www.linkedin.com/in/artur-bondar/">Artur Bondar</a>, back-end, front-end</p>
+        <p><img src={AntonP} width="150" height="150" alt="AntonP image"/></p> 
+        <p><a href="https://www.linkedin.com/in/anton-podolian/">Anton Podolian</a>, front-end</p>
+        <p><img src={Dmytro} width="150" height="150" alt="Dmytro image"/></p>
+        <p><a href="https://www.linkedin.com/in/%D0%B4%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9-%D1%81%D0%B5%D0%BC%D1%91%D0%BD%D0%BE%D0%B2-3686371aa/">Dmytro Semenov</a>, front-end</p>
+        <p><img src={Nastia} width="150" height="150" alt="Nastia image"/></p> 
+        <p><a href="https://www.linkedin.com/in/anastasiia-gvozdieva-6ba2161a2/">Anastasiia Gvozdieva</a>, front-end</p>
+        <p><img src={AntonH} width="150" height="150" alt="AntonH image"/></p> 
+        <p><a href="https://www.linkedin.com/in/anton-horbachov/">Anton Horbachov</a>, front-end</p>
+        <p><img src={Serega} width="150" height="150" alt="Serega image"/></p> 
+        <p><a href="https://www.linkedin.com/in/sergey-tikunov-939009150/">Sergey Tikunov</a>, front-end</p>
+        
         <div className="Images-Wrapper">
         <h4>Other photos of our perfect team:</h4>
         <img src={GR1} width="700" height="500" alt="GR1 image"/>
@@ -35,8 +57,11 @@ class About extends Component {
         <p></p>
         <img src={GR5} width="700" height="500" alt="GR5 image"/>
         <p></p>
-        <h4>Our teacher:</h4>
+        <h4>Alexandr Ordinskiy, our teacher:</h4>
         <img src={GR6} width="400" height="500" alt="GR6 image"/>
+
+        <h4>Goodbye!</h4>
+        
         </div>
        
         
